e5907403ad2aa87c0ccd27c5f8c4accdd2e3f95d
severoiuliano@gmail.com <severoiuliano@gmail.com>
2018-05-08 20:20:43 +0200

    Initial release for testing

---
+++ ImagecreateShortcode.php
@@ -0,0 +1,202 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class ImagecreateShortcode extends Shortcode
+{
+    protected $fontname;
+
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('ic', array($this, 'process'));
+    }
+
+    public function process(ShortcodeInterface $sc)
+    {
+        $fn = $sc->getParameter('file', null);
+        if ($fn === null) {
+            $fn = $sc->getShortcodeText();
+            $fn = str_replace('[ic=', '', $fn);
+            $fn = str_replace('/]', '', $fn);
+            $fn = str_replace(']', '', $fn);
+            $fn = trim($fn);
+        }
+        if ( ($fn === null) && ($fn === '') ) {
+            return "<p>Image Create: Malformed shortcode (<tt>".htmlspecialchars($sc->getShortcodeText())."</tt>).</p>";
+        }
+
+        // Gets the parameters entered on the markdown page
+        $string     = $sc->getParameter('string', null);
+        $fontsize   = $sc->getParameter('fontsize', null);
+        
+        $falpha = $sc->getParameter('falpha', null);
+        $falpha = 127 - ( $falpha * 127 );
+        $fore_color = $sc->getParameter('fcolor', null);
+        $fcolor = $this->hex2rgba($fore_color, true);
+        $afcolor = sscanf($fcolor, "rgba(%d, %d, %d, %f)");
+
+        $balpha = $sc->getParameter('balpha', null);
+        $balpha = 127 - ( $balpha * 127 );
+        $back_color = $sc->getParameter('bcolor', null);
+        $bcolor = $this->hex2rgba($back_color, true);
+        $abcolor = sscanf($bcolor, "rgba(%d, %d, %d, %f)");
+        
+        $width      = $sc->getParameter('width', null);
+        $height     = $sc->getParameter('height', null);
+        $image_type = $sc->getParameter('image_type', null);
+        $quality    = $sc->getParameter('quality', null);
+        $position   = $sc->getParameter('position', null);
+
+        $demo = $sc->getParameter('demo', null);
+        if ($demo) {
+			$shortcode = $sc->getShortcodeText();
+			$shortcode = str_replace(' demo="true"','',$shortcode);
+			return "<tt>".htmlspecialchars($shortcode)."</tt>";
+		}
+
+        // Check if the variables contain useful values otherwise
+        // they are taken from the plugin configuration file
+        if ($string === null) $string = $this->config->get('plugins.imagecreate.parameters.string');
+        if ($width === null) $width = $this->config->get('plugins.imagecreate.parameters.width');
+        if ($height === null) $height = $this->config->get('plugins.imagecreate.parameters.height');
+        if ($position === null) $position = $this->config->get('plugins.imagecreate.parameters.position');
+        if ($fontsize === null) $fontsize = $this->config->get('plugins.imagecreate.parameters.fontsize');
+        if ($image_type == null) $image_type = $this->config->get('plugins.imagecreate.parameters.image_type');
+
+        if ($fore_color === null) {
+            $fcolor = $this->config->get('plugins.imagecreate.parameters.foreground_color');
+            $afcolor = sscanf($fcolor, "rgba(%d, %d, %d, %f)");
+            $falpha = 127 - ( $afcolor[3] * 127 );
+            
+        }
+        if ($back_color === null) {
+            $bcolor = $this->config->get('plugins.imagecreate.parameters.background_color');
+            $abcolor = sscanf($bcolor, "rgba(%d, %d, %d, %f)");
+            $balpha = 127 - ( $abcolor[3] * 127 );
+        }
+        if ($quality === null) $quality = $this->config->get('plugins.imagecreate.parameters.quality');
+
+        $output = '';
+
+        // Set the enviroment variable for GD
+        putenv('GDFONTPATH=' . realpath('./user/plugins/imagecreate/fonts/'));
+
+        // Name the font to be used, unique for now
+        $this->fontname = 'Capriola-Regular';
+
+        // Calculates and returns the bounding box in pixels for a FreeType text.
+        $box = imageftbbox( $fontsize, 0, $this->fontname, $string );
+        $w = abs($box[0])+abs($box[2])+10;
+        $h = abs($box[1])+abs($box[7])+6;
+
+        // Create a new palette based image
+        $image = imagecreate( $w, $h );
+
+        // Allocate a color for an image
+        $text_color = imagecolorallocatealpha( $image, $afcolor[0], $afcolor[1], $afcolor[2], $falpha );
+        $background = imagecolorallocatealpha( $image, $abcolor[0], $abcolor[1], $abcolor[2], $balpha );
+
+        // Draw a filled rectangle
+        imagefilledrectangle( $image, 0, 0, $w, $h, $background );
+
+        // Calculates the points where you place text to center it
+        // horizontally and vertically into an image.
+        $x = ($w - ($box[2] - $box[0])) / 2; 
+        $y = ($h - ($box[1] - $box[7])) / 2;
+        $y -= $box[7];
+
+        $angle = 0;
+
+        // Write text to the image using TrueType fonts
+        imagettftext($image, $fontsize, $angle, $x, $y, $text_color, $this->fontname, $string);
+
+        /*
+         * start a new output buffer
+         * generate the image
+         * grab the buffer contents
+         * stop this output buffer
+         */
+        ob_start();
+        switch ($image_type) {
+            case 'gif':
+                imagegif( $image, NULL);
+                break;
+            case 'jpeg':
+                imagejpeg( $image, NULL, $quality );
+                break;
+            case 'png':
+                imagepng( $image, NULL, $quality );
+                break;
+        }
+        $contents = ob_get_contents();
+        ob_end_clean();
+
+        // De-allocate a color for an image
+        imagecolordeallocate($image, $background );
+        imagecolordeallocate($image, $text_color );
+
+        // frees any memory associated with image
+        imagedestroy($image);
+
+        // Image Position: centered, left, right
+        $style = "style='STYLE'";
+        switch ($position) {
+            case 'centered':
+                $style = str_replace('STYLE','display: block;margin-left: auto;margin-right: auto;',$style);
+                break;
+            case 'left':
+                $style = str_replace('STYLE','display: block;margin-right: auto;',$style);
+                break;
+            case 'right':
+                $style = str_replace('STYLE','display: block;margin-left: auto;',$style);
+                break;
+        }
+
+        $style= "style='vertical-align:middle;'";
+        // Returns the output to display
+        $output = "<img src='data:image/".$image_type.";base64,".base64_encode($contents)."' ".$style."/>";
+        return $output;
+    }
+
+
+    /* Convert hexdec color string to rgb(a) string */
+	public function hex2rgba($color, $opacity = false)
+	{
+		$default = 'rgb(0,0,0)';
+
+		//Return default if no color provided
+		if(empty($color))
+		return $default; 
+
+		//Sanitize $color if "#" is provided 
+		if ($color[0] == '#' ) {
+			$color = substr( $color, 1 );
+		}
+
+		//Check if color has 6 or 3 characters and get values
+		if (strlen($color) == 6) {
+			$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+		} elseif ( strlen( $color ) == 3 ) {
+			$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+		} else {
+			return $default;
+		}
+
+		//Convert hexadec to rgb
+		$rgb =  array_map('hexdec', $hex);
+
+		//Check if opacity is set(rgba or rgb)
+		if($opacity){
+			if(abs($opacity) > 1)
+				$opacity = 1.0;
+			$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
+		} else {
+			$output = 'rgb('.implode(",",$rgb).')';
+		}
+
+		//Return rgb(a) color string
+		return $output;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,97 @@
+# Imagecreate Plugin
+
+The ![](images/ic1.png) Plugin is for [Grav CMS](http://github.com/getgrav/grav). Create a new image with a text using TrueType fonts.
+
+## Installation
+
+Installing the Imagecreate plugin can be done in one of two ways. The GPM (Grav Package Manager) installation method enables you to quickly and easily install the plugin with a simple terminal command, while the manual method enables you to do so via a zip file.
+
+### GPM Installation (Preferred)
+
+The simplest way to install this plugin is via the [Grav Package Manager (GPM)](http://learn.getgrav.org/advanced/grav-gpm) through your system's terminal (also called the command line).  From the root of your Grav install type:
+
+    bin/gpm install imagecreate
+
+This will install the Imagecreate plugin into your `/user/plugins` directory within Grav. Its files can be found under `/your/site/grav/user/plugins/imagecreate`.
+
+### Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `imagecreate`. You can find these files on [GitHub](https://github.com/severo-iuliano/grav-plugin-imagecreate) or via [GetGrav.org](http://getgrav.org/downloads/plugins#extras).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/imagecreate
+
+> NOTE: This plugin is a modular component for Grav which requires [Grav](http://github.com/getgrav/grav) and the [Error](https://github.com/getgrav/grav-plugin-error) and [Problems](https://github.com/getgrav/grav-plugin-problems) to operate.
+
+## Configuration
+
+Before configuring this plugin, you should copy the `user/plugins/imagecreate/imagecreate.yaml` to `user/config/plugins/imagecreate.yaml` and only edit that copy.
+
+Here is the default configuration and an explanation of available options:
+
+```yaml
+enabled: true
+string: email@example.com
+fontsize: 10
+foreground_color: 'rgba(156, 29, 61, 1)'
+background_color: 'rgba(249, 242, 244, 1)'
+width: 210
+height: 27
+image_type: png
+quality: 6
+position: centered
+```
+
+## Usage
+
+This plugin creates a new image with a text using TrueType fonts.
+
+### Creating an Image
+
+This plugin uses the [Shortcode Core](https://github.com/getgrav/grav-plugin-shortcode-core) infrastructure. Read those docs for the nitty gritty of how shortcodes work.
+
+The `Imagecreate` shortcode is a self-closing `[ic option1="value1" option2="value2" ... /]`, and does not have any mandatory parameters, but it would not be useful to use it in this mode.
+
+It, with greater profit, accepts the following options:
+
+* `string` contains the text to display. If nothing is passed, the default text will be `email@example.com`.
+
+* `fontsize` lets you assign the fontsize.
+
+* `fcolor` is the color in the foreground.
+
+* `falpha` is a value between 0 and 1. 0 indicates completely transparent while 1 indicates completely opaque.
+
+* `bcolor` is the color in the background.
+
+* `balpha` is a value between 0 and 1. 0 indicates completely transparent while 1 indicates completely opaque.
+
+* `width` lets you assign the image width. It accepts an integer from 50 to 500 and the default value is 210.
+
+* `height` lets you assign the image height. It accepts an integer from 20 to 500 and the default value is 27.
+
+* `quality` is compression level: from 0 (no compression) to 9. The current default is 6.
+
+* `position` tells where you can place the image: left, right or centered.
+
+### Example Code
+
+Below is an example without any parameters, one with only the string parameter and last with all the possible parameters.
+
+|  |  |  |
+|---|---|---|
+| `[ic demo="true"]` | ![](images/ic2.png) |
+| `[ic demo="true" string="My text to display"/]` | ![](images/ic3.png) |
+| `[ic demo="true" string="Hi GRAV people!" width="520" height="150" position="centered" fontsize="40" fcolor="006300" bcolor="f0f000" falpha="0.5" balpha="1" quality="6"]` | ![](images/ic4.png) |
+
+## Credits
+
+To work the plugin requires a [shortcode-code plugin](https://github.com/getgrav/grav-plugin-shortcode-core): a sincere thanks to the Grav team.
+
+## To Do
+
+- [ ] Verify placement of text within the image area when the `angle` is different from zero
+- [ ] Add new fonts
+- [ ] Create an image with text from an existing image from a file or URL
+
